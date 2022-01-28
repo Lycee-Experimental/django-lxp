f2effe5ec22f521e523629044fa3a93cfb487abd
davy39 <davy@ik.me>
2022-01-28 18:20:46 +0100

    Encore un test setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,8 @@
-#import subprocess
-#import sys
-#subprocess.check_call([sys.executable, "-m", "pip", "install", "-r", "requirements.txt"])
-pass
+import subprocess
+import sys
+from setuptools import setup
+subprocess.check_call([sys.executable, "-m", "pip", "install", "-r", "requirements.txt"])
+setup(
+    name="django-lxp",
+    version="1.0",
+)
