4cd59d2edb5006af46ec6e631e5e30058f925e9c
davy39 <davy@ik.me>
2022-01-28 18:14:02 +0100

    test empty setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
-import subprocess
-import sys
-subprocess.check_call([sys.executable, "-m", "pip", "install", "-r", "requirements.txt"])
+#import subprocess
+#import sys
+#subprocess.check_call([sys.executable, "-m", "pip", "install", "-r", "requirements.txt"])
+pass
