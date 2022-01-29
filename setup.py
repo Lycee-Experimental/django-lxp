5ba021deae7bbdbe142d6e9e934fb2a893e42bc8
davy39 <davy@ik.me>
2022-01-29 13:24:42 +0100

    Passage a Pipenv

--- setup.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-import sys
-from setuptools import setup
-subprocess.check_call([sys.executable, "-m", "pip", "install", "-r", "requirements.txt"])
-setup(
-    name="django-lxp",
-    version="1.0",
-)
