ba6398ad9752ec730ab2e2ed776e82fbec15ae7c
davy39 <davy@ik.me>
2022-02-10 12:58:45 +0100

    Ajout switch ancien/nouveau + GB/ECCO 2

--- djangoLxp/urls.py
+++ djangoLxp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from django.contrib import admin
 from django.urls import re_path as url
 from inscription.models import Commune
-from inscription.views import AutocompleteCommune, AutocompletePays, AutocompleteDepartement
+from inscription.views import AutocompleteCommune, AutocompletePays, AutocompleteDepartement, AutocompleteMEE
 
 urlpatterns = [
     path('admin/', admin.site.urls),
