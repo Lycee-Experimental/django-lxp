db564858a0c6c9308b082f2c1439e7e9c09d009b
Davy39 <davy@ik.me>
2022-12-24 14:12:22 +0100

    Suppression -> Désactivation

--- static/js/hide_resp2.js
+++ static/js/hide_resp2.js
@@ -1,19 +1,21 @@
 // Fonction permettant de cacher les champs inutiles du resp2
 function Hide() {
     if(document.getElementById('id_1-resp2').options[document.getElementById('id_1-resp2').selectedIndex].value == "aucun") {
-    document.getElementById('div_id_1-nom_resp2').style.display = 'none';
-    document.getElementById('div_id_1-prenom_resp2').style.display = 'none';
-    document.getElementById('div_id_1-adresse_resp2').style.display = 'none';
-    document.getElementById('div_id_1-tel_resp2').style.display = 'none';
-    document.getElementById('div_id_1-email_resp2').style.display = 'none';
-    document.getElementById('div_id_1-sociopro_resp2').style.display = 'none';
+        $("#id_1-nom_resp2").prop("disabled", true);
+        $("#id_1-civilite_resp2").prop("disabled", true);
+        $("#id_1-prenom_resp2").prop("disabled", true);
+        $("#id_1-adresse_resp2").prop("disabled", true);
+        $("#id_1-tel_resp2").prop("disabled", true);
+        $("#id_1-email_resp2").prop("disabled", true);
+        $("#id_1-sociopro_resp2").prop("disabled", true);
     } else {
-        document.getElementById('div_id_1-nom_resp2').style.display = '';
-        document.getElementById('div_id_1-prenom_resp2').style.display = '';
-        document.getElementById('div_id_1-adresse_resp2').style.display = '';
-        document.getElementById('div_id_1-tel_resp2').style.display = '';
-        document.getElementById('div_id_1-email_resp2').style.display = '';
-        document.getElementById('div_id_1-sociopro_resp2').style.display = '';
+        $("#id_1-nom_resp2").prop("disabled", false);
+        $("#id_1-civilite_resp2").prop("disabled", false);
+        $("#id_1-prenom_resp2").prop("disabled", false);
+        $("#id_1-adresse_resp2").prop("disabled", false);
+        $("#id_1-tel_resp2").prop("disabled", false);
+        $("#id_1-email_resp2").prop("disabled", false);
+        $("#id_1-sociopro_resp2").prop("disabled", false);
     }
 }
 // Vérifie id_1-resp2 est à aucun (fonction Hide) après le chargement du document.
