047d2b9f2f887bd680a40b1651a866b01f73cbd2
davy39 <davy@ik.me>
2023-03-04 18:09:44 +0100

    Hide desco

--- static/js/hide_lxp_an_passe.js
+++ static/js/hide_lxp_an_passe.js
@@ -1,5 +1,5 @@
 // Fonction permettant de cacher les champs inutiles du resp2
-function Hide() {
+function HideAncien() {
     if(document.getElementById('ancien').checked) {
         $("#id_2-niveau_an_passe").prop("disabled", false);
         $("#id_2-gb_an_passe").prop("disabled", false);
@@ -15,12 +15,22 @@ function Hide() {
         $("#id_2-mee_entretien").prop("disabled", false);
     }
 }
+function HideDesco() {
+    if(document.getElementById('desco').checked) {
+        $("#id_2-etablissement_origine").prop("disabled", true);
+    } else {
+        $("#id_2-etablissement_origine").prop("disabled", false);
+    }
+}
 // Vérifie id_1-resp2 est à aucun (fonction Hide) après le chargement du document.
 // Utile lors du retour en arrière dans le formulaire
 document.addEventListener("DOMContentLoaded", function() {
-    Hide();
+    HideAncien();
+    HideDesco();
 });
 // Appelle la fonction quand on change le statut de resp2
 window.onload = function() {
-    document.getElementById('ancien').onchange = Hide;
+    document.getElementById('ancien').onchange = HideAncien;
+    document.getElementById('desco').onchange = HideDesco;
+
 };
